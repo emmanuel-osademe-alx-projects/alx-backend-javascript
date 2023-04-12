1047e8b9019e731544efc8cfbfb30f527c188332
emmanuel <mcdecoil@gmail.com>
2023-04-12 15:10:08 +0000

    okat 1

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Funtion to display a message on the STDOUT.
+ * @param {String} message Message to display.
+ */
+
+const displayMessage = (message) => {
+	console.log(message);
+};
+
+module.exports = displayMessage;
