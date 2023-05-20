4d9a5aaef077ed2efac62f3bc50d36e422887eae
Emmanuel Osademe ALX Projects <mcdecoil@gmail.com>
2023-05-20 01:39:05 +0100

    Update 0-console.js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,10 @@
-function displayMessage (data) {
-  console.log(data);
-}
+/**
+ * Function to displays message on the STDOUT.
+ * @param {String} msg: The message to display.
+ */
+
+const displayMessage = (msg) => {
+  console.log(msg);
+};
 
 module.exports = displayMessage;
